72c736f3acfcffdd67542bf2e683f7f9b936bec3
unknown <stolyarova.199535@gmail.com>
2023-09-13 10:41:36 +0300

    upweb

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './build'),
     filename: production ? '[name].[contenthash].js' : '[name].js',
+    clean: true,
   },
   module: {
     rules: [
@@ -47,7 +48,6 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      base: '/'
     }),
   ],
   devtool: 'inline-source-map',
